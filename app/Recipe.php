616c478e36081da2f26ea30cdd33364a5b09a4a3
Matej Čičak <matej.cicak44@gmail.com>
2017-10-26 21:07:09 +0200

    migracije, mmodeli i view prikaz svih recepata

---
+++ app/Recipe.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Recipe extends Model
+{
+    public function ingredients() {
+		return $this->hasMany('App\Ingredient');
+	}
+	
+	public function creator() {
+		return $this->belongsTo('App\User');
+	}
+}
