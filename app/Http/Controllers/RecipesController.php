a335f664ea98d69800958078f936d31da3f3a571
Matej Čičak <matej.cicak44@gmail.com>
2017-11-08 23:35:19 +0100

    promjene u controlleru

--- app/Http/Controllers/RecipesController.php
+++ app/Http/Controllers/RecipesController.php
@@ -64,7 +64,7 @@ class RecipesController extends Controller
      */
     public function edit($id)
     {
-        return "Promjena recepta sa ID: " . $id;  //prikaz web obrasca
+        return view ('edit')->with('recipe', Recipe::find($id));
     }
 
     /**
@@ -74,9 +74,9 @@ class RecipesController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(Request $request)
     {
-        return "Spremanje promjena recepta ID: " . $id;
+        return "Spremanje promjena recepta ID:";
     }
 
     /**
