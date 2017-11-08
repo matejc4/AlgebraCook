4a550a599265beb918ea5d43e3c7c8f203afc4c6
Matej Čičak <matej.cicak44@gmail.com>
2017-11-08 23:28:47 +0100

    promjene na index page ispravak

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,8 +4,8 @@
 
 @section('content')
 <div class="container">
-	<div class="col-md col-md-offset-1">
-		<div class="panel pnale-default">
+	<div class="col-md-10 col-md-offset-1">
+		<div class="panel panel-default">
 			<div class="panel-heading">Recepti<br>
 				<a href="{{ url('/recipes/add') }}">
 					<i class="fa fa-btn fa-plus"></i>
@@ -17,7 +17,7 @@
 					@foreach ($recipes as $recipe)
 					<li class="list-group-item">
 						<a href="recipes/view/{{ $recipe->id }}">{{ $recipe->name }}</a>
-					<li>
+					</li>
 					@endforeach
 				</ul>
 			</div>
