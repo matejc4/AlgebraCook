38364c0b0a18892e6a401f28adc18072b1d61df7
Matej Čičak <matej.cicak44@gmail.com>
2017-11-09 16:00:05 +0100

    promjene na route

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,5 +1,4 @@
 <?php
-
 /*
 |--------------------------------------------------------------------------
 | Application Routes
@@ -10,32 +9,23 @@
 | and give it the controller to call when that URI is requested.
 |
 */
-
-//authentication routes
-Route::get('/auth', 'Auth\AuthController@getLogin');
-Route::post('/auth', 'Auth\AuthController@postLogin');
-Route::get('/auth', 'Auth\AuthController@getLogout');
-
-//registration routes
-Route::get('/auth', 'Auth\AuthController@getRegister');
-Route::post('/auth', 'Auth\AuthController@postRegister');
-
+// Authentication routes
+Route::get('/auth/login', 'Auth\AuthController@getLogin');
+Route::post('/auth/login', 'Auth\AuthController@postLogin');
+Route::get('/auth/logout', 'Auth\AuthController@getLogout');
+// Registration routes
+Route::get('/auth/register', 'Auth\AuthController@getRegister');
+Route::post('/auth/register', 'Auth\AuthController@postRegister');
 Route::get('/', function () {
     return redirect('/recipes');
 });
-
 Route::get('/home', function () {
     return redirect('/recipes');
 });
-
-Route::get ('/recipes', 'RecipesController@index'); //prikaz svih recepata
-
-Route::get ('/recipes/add', 'RecipesController@add'); //prikaz view-a sa web obrascem za unos
-Route::post('/recipes/add', 'RecipesController@save'); //spremanje podataka u bazu
-
-Route::get('/recipes/view/{id}', 'RecipesController@view'); //detaljni prikaz određenog recepta
-
-Route::get('/recipes/edit/{id}', 'RecipesController@edit'); //prikaz weborasca za uređivanje
-Route::post('/recipes/edit', 'RecipesController@update'); //ažuriranje podataka u bazi
-
-Route::delete('/recipes/del/{id}', 'RecipesController@delete'); //brisanje recepta
+Route::get('/recipes', 'RecipesController@index'); // prilaz svih recepata (popis)
+Route::get('/recipes/add', 'RecipesController@add'); // prikaz view-a sa web obrascem za unos
+Route::post('/recipes/add', 'RecipesController@save'); // spremanje podataka u bazu
+Route::get('/recipes/view/{id}', 'RecipesController@view'); // detaljan prikaz određenog recepta
+Route::get('/recipes/edit/{id}', 'RecipesController@edit'); // prikaz webobrasca za uređivanje
+Route::post('/recipes/edit', 'RecipesController@update'); // ažuriranje podataka u bazi
+Route::delete('recipes/del/{id}', 'RecipesController@delete'); // brisanje recepta
